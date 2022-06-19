340e50d4d7ce28de7977028401e34b5525e46539
krrish <87867238+krrish-v@users.noreply.github.com>
2022-06-19 17:43:55 +0530

    Update databse.py

--- BLOCK/databse.py
+++ BLOCK/databse.py
@@ -74,7 +74,7 @@ class get_in_data_user():
         self.conn_ = sqlite3.connect('backend/BLOCK/database/data_user.db')
 
     def insert_patient(self, dsakey, api, token, password, firstname, lastname, address, country, state, zip_code, phone_no, status, date): #random_verifying_code
-        if 1==1:#try:
+        try:
             cursor = self.conn_.cursor()
             
             #cursor.execute(
@@ -89,10 +89,10 @@ class get_in_data_user():
             self.conn_.close()
             return True
         
-        #except: return False
+        except: return False
 
     def check_patient_info(self, phone, passwd):
-        if 1==1:#try:
+        try:
             cursor = self.conn_.cursor()
             com = "select * from hc_patient where phone_no = " + phone
             cursor.execute(com)
@@ -114,7 +114,7 @@ class get_in_data_user():
             
             else: return False
 
-        #except: return False
+        except: return False
     
     def update_patient(self, phone_no, field, new_value):
         """
@@ -133,7 +133,7 @@ class get_in_data_user():
         except: return False
     
     def insert_doctor(self, dsakey, api, token, email, password, firstname, lastname, address, country, state, zip_code, phone_no, about, specialist_in, status, date): #random_verifying_code
-        if 1==1:#try:
+        try:
             cursor = self.conn_.cursor()
             
             #cursor.execute(
@@ -148,7 +148,30 @@ class get_in_data_user():
             self.conn_.close()
             return True
         
-        #except: return False
+        except: return False
+
+    def get_doctor(self):
+        try:
+            cursor = self.conn_.cursor()
+            cursor.execute('select token, firstname, lastname, country, specialist_in, about from hc_doctor')
+            data = cursor.fetchall()
+            self.conn_.close()
+
+            if len(data) != 0:
+                all_therapist = {}
+                for i in data:
+                    tokn = i[0]
+                    user = {tokn: {
+                        'name': i[1]+' '+i[2],
+                        'country': i[3],
+                        'specialist': i[4],
+                        'about': i[5]
+                    }}
+                    all_therapist.update(user)
+                return all_therapist
+
+        except:
+            return False
 
     def check_doctor_info(self, passwd, email=None, phone=None):
         if email is None: com = "select * from hc_doctor where phone_no = " + phone
@@ -219,3 +242,29 @@ class user_api():
             else: return False
 
         except: return None
+
+
+#import time
+#tmt = time.ctime()
+#password = blk.hash384('fdewf23c')
+
+#print(get_in_data_user().insert_doctor('ewfr3283iuqw38r 2iugewhcvefir32fywejhf3r2ifyejh', 'retrdfvdsfbnythtgrferht', 'rfewfcesv32rdwffw32d', 'harry@gmail.com', password, 'harry', 'poter', 'New Delhi Sector 4', 'India', 'New Delhi', '7353', '99999999999', 'i have 2 year of experience', 'mental_health', True, str(tmt)))
+#print(get_in_data_user().load_doctor())
+
+#print(get_in_data_user().get_doctor())
+#print(get_in_data_user().insert_patient('hvfyu23qwhjvcf3rg2uifekig2 fevwhfbcuduiejhi4ew', 'ehf78iu4378irewvhcig4f', 'rfewfcesv32rdwffw32d', password, 'harry', 'poter', 'New Delhi Sector 4', 'India', 'New Delhi', '7353', '99999999999', True, str(tmt)))
+#print(get_in_data_user().load_patient())
+
+#print(get_in_data_user().check_patient_info('99999999999', '8cfbace8e8c053bc56d00f97a8bb029db8b4bdae71cf31bca0b7b1ea84fdc7761f126ce3e8c014083bab43be9dec42a2'))
+#print(get_in_data_user().check_doctor_info('8cfbace8e8c053bc56d00f97a8bb029db8b4bdae71cf31bca0b7b1ea84fdc7761f126ce3e8c014083bab43be9dec42a2', email='harry@gmail.com'))
+#import time
+#tmt = time.ctime()
+#print(get_in_data().update("123","api","100"))
+#print(get_in_data().load())
+
+#ap = create_api().api()
+#q = user_api().insert(ap)
+#print(q)
+#print(ap)
+#t = user_api().check(ap)
+#print(t)
